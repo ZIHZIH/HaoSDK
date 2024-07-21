45ed0f335783aa19897dda4cc51778e7a0063100
Wang Zihao <wangzihao@qimao.com>
2024-07-21 21:25:04 +0800

    增加作者微信号和qq号

--- author.go
+++ author.go
@@ -3,3 +3,11 @@ package HaoSDK
 func GetAuthorName() string {
 	return "王自豪"
 }
+
+func GetAuthorWechat() string {
+	return "wzh2036844072"
+}
+
+func GetAuthorQQ() string {
+	return "3327619269"
+}
